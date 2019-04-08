cdad35e2c2ba808329c16426c50c93dc2ea25a18
Frank Villaro-Dixon <frank@vi-di.fr>
2019-04-08 14:08:38 +0200

    Stash changes

--- util/hc-sr04.c
+++ util/hc-sr04.c
@@ -11,15 +11,13 @@
 
 int TRIG, ECHO;
 
-static int ping()
+float ping()
 {
-	long ping      = 0;
-	long pong      = 0;
+	unsigned long ping      = 0;
+	unsigned long pong      = 0;
 	float distance = 0;
- 	long timeout   = 500000; // 0.5 sec ~ 171 m
+ 	long timeout   = micros() + 1000000; // 0.5 sec ~ 171 m
 	
-	pinMode(TRIG, OUTPUT);
-	pinMode(ECHO, INPUT);
 	
 	// Ensure trigger is low.
 	digitalWrite(TRIG, LOW);
@@ -33,56 +31,76 @@ static int ping()
 	// Wait for ping response, or timeout.
 	while (digitalRead(ECHO) == LOW && micros() < timeout) {
 	}
-	
-	// Cancel on timeout.
-	if (micros() > timeout) {
-		printf("Out of range.\n");
-		return 0;
-	}
-	
 	ping = micros();
 	
 	// Wait for pong response, or timeout.
 	while (digitalRead(ECHO) == HIGH && micros() < timeout) {
 	}
-	
-	// Cancel on timeout.
-	if (micros() > timeout) {
-		printf("Out of range.\n");
-		return 0;
-	}
-	
 	pong = micros();
 	
 	// Convert ping duration to distance.
 	distance = (float) (pong - ping) * 0.017150;
+
+
+	return distance;
 	
-	printf("Distance: %.2f cm.\n", distance);
+//	printf("%.2f\n", distance);
 	
-	return 1;
+//	return 1;
+}
+
+float median(float *x, int n) {
+    float temp;
+    int i, j;
+    // the following two loops sort the array x in ascending order
+    for(i=0; i<n-1; i++) {
+        for(j=i+1; j<n; j++) {
+            if(x[j] < x[i]) {
+                // swap elements
+                temp = x[i];
+                x[i] = x[j];
+                x[j] = temp;
+            }
+        }
+    }
+
+    if(n%2==0) {
+        // if there is an even number of elements, return mean of the two elements in the middle
+        return((x[n/2] + x[n/2 - 1]) / 2.0);
+    } else {
+        // else return the element in the middle
+        return x[n/2];
+    }
 }
 
 int main (int argc, char *argv[])
 {
-	if (argc != 3) {
-		printf ("usage: %s <trigger> <echo>\n\nWhere:\n- trigger is the wiringPi trigger pin number.\n- echo is the wiringPi echo pin number.\nUsing trigger %d and echo %d.\n", argv[0], argv[1], argv[2]);
-	} else {
-		TRIG = atoi(argv[1]);
-		ECHO = atoi(argv[2]);
-	}
-	
-	printf ("Raspberry Pi wiringPi HC-SR04 Sonar test program.\n");
 
+	TRIG = 4;
+	ECHO = 5;
+	
 	if (wiringPiSetup () == -1) {
 		exit(EXIT_FAILURE);
 	}
-		
-	if (setuid(getuid()) < 0) {
-		perror("Dropping privileges failed.\n");
-		exit(EXIT_FAILURE);
+
+	pinMode(TRIG, OUTPUT);
+	pinMode(ECHO, INPUT);
+
+	int num_tries = 800;
+	float *tries = malloc(sizeof(*tries) * num_tries);
+	for(int i = 0; i < num_tries; i++) {
+		float d = 0;
+		while(d < 7) {
+			delay(15);
+			d = ping();
+		}
+
+		tries[i] = d;
+		//printf("%.2f\n", tries[i]);
 	}
-	
-	ping();
+
+	printf("%.2f\n", median(tries, num_tries));
+		
 	
 	return 0;
 }
